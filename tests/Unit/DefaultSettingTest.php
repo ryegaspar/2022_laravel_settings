3eb7c5587bc9d145116cd038834b1b758c52766f
rye gaspar <gaspar.ryan@outlook.com>
2022-06-01 21:03:19 -0500

    add renaming default setting test

--- tests/Unit/DefaultSettingTest.php
+++ tests/Unit/DefaultSettingTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Models\Account;
 use App\Models\AccountType;
 use App\Models\DefaultSetting;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -14,8 +15,26 @@ class DefaultSettingTest extends TestCase
 	/** @test */
     public function it_belongs_to_account_type()
     {
-		$setting = DefaultSetting::find(1);
+		$defaultSetting = DefaultSetting::find(1);
 
-		$this->assertInstanceOf(AccountType::class, $setting->accountType);
+		$this->assertInstanceOf(AccountType::class, $defaultSetting->accountType);
     }
+
+	/** @test */
+	public function renaming_name_does_not_affect_individual_settings()
+	{
+		$account = Account::factory()->create([
+			'account_type_id' => 2
+		]);
+
+		$this->assertEquals('nike', $account->settings->brand);
+
+		// default setting for vendor, brand as the name
+		$defaultSetting = DefaultSetting::find(4);
+		$defaultSetting->update([
+			'name' => 'new_brand_name'
+		]);
+
+		$this->assertEquals('nike', $account->fresh()->settings->new_brand_name);
+	}
 }
